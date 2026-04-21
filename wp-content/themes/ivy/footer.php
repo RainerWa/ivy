f99814772e37c335f7508219b53f100f68760e9b
Rainer <wachtel@technoid.de>
2026-04-21 16:14:22 +0200

    footer.php

---
+++ wp-content/themes/ivy/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Custom Footer Template for Ivy Child Theme
+ */
+
+defined('ABSPATH') || exit;
+?>
+
+</div>
+</div>
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="col-full">
+
+        <div class="site-info">
+            © ivy 2026
+            <br>
+
+    </div><!-- .col-full -->
+</footer>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
+
