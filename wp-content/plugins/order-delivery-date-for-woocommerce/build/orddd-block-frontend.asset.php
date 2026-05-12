37161eec5a4f342150c16bc6ee5e2d73204563c6
Rainer <wachtel@technoid.de>
2026-05-12 15:52:50 +0200

    update

---
+++ wp-content/plugins/order-delivery-date-for-woocommerce/build/orddd-block-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-settings', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ded14adcec7169e50367');
---
+++ wp-content/plugins/order-delivery-date-for-woocommerce/build/orddd-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '257a326e641bb5665025');
